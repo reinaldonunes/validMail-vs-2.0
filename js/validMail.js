d5e2815351299b77ebec575823f938fb10274fd5
Reinaldo J. Nunes <reinaldo.nunes2@gmail.com>
2015-10-08 10:40:52 -0300

    correction cpf validation, identifying repeated or missing characters.

--- js/validMail.js
+++ js/validMail.js
@@ -83,34 +83,57 @@
 				
 				// CPF Validation
 				if(idCampo == 'CPF:'){ 
-					var cpf = $(this).val();
-					cpf = cpf.replace(/[^\d]+/g,'');    
-					   
-					if (cpf.length != 11 ||	cpf == "00000000000" ||	cpf == "11111111111" ||	cpf == "22222222222" || cpf == "33333333333" || cpf == "44444444444" || cpf == "55555555555" ||	cpf == "66666666666" ||	cpf == "77777777777" || cpf == "88888888888" || cpf == "99999999999"){
-						alert("cpf não válido");
-						vazio++;
-						return false;
-					}       
+					var c = $(this).val();
+					c = c.replace(/[^\d]+/g,'');
+					var cpf = c;
 					var i;
-					var add; 
-					for (i=0;i < 9; i ++){       
-						add += parseInt(cpf.charAt(i)) * (10 - i);  
-						rev = 11 - (add % 11);  
-						if (rev == 10 || rev == 11) rev = 0;    
-						if (rev != parseInt(cpf.charAt(9))) return false;
+					var s;
+					s = c;
+					c = s.substr(0,9);
+					var dv = s.substr(9,2);
+					var d1 = 0;
+					var v = false;
 					
-						add = 0;    
-						for (i = 0; i < 10; i ++){      
-							add += parseInt(cpf.charAt(i)) * (11 - i);  
-							var rev = 11 - (add % 11);  
-							if (rev == 10 || rev == 11) rev = 0;    
-							if (rev != parseInt(cpf.charAt(10))) return false;      
-							
+					if(cpf == '11111111111' ||	cpf == '22222222222' || cpf == '33333333333' || cpf == '44444444444' || cpf == '55555555555' || cpf == '66666666666' || cpf == '77777777777' || cpf == '88888888888' || cpf == '99999999999' || cpf == '00000000000'){
+							  '11111111111'
+						$(settings.display).html("Invalid CPF!");
+						vazio++;
+						v = true;
+						return false;
+					}else{
+						for (i = 0; i < 9; i++){
+							d1 += c.charAt(i)*(10-i);
+						}
+					
+						if (d1 == 0){
+							$(settings.display).html("Invalid CPF!");
+							vazio++;
+							v = true;
+							return false;
+						}
+						d1 = 11 - (d1 % 11);
+						if (d1 > 9) d1 = 0;
+						if (dv.charAt(0) != d1){
+							$(settings.display).html("Invalid CPF!");
+							v = true;
+							vazio++;
+							$(this).focus();
+							return false;
+						}
+				
+						d1 *= 2;
+						for (i = 0; i < 9; i++){
+							d1 += c.charAt(i)*(11-i);
+						}
+						d1 = 11 - (d1 % 11);
+						if (d1 > 9) d1 = 0;
+						if (dv.charAt(1) != d1){
 							vazio = 0;
-							alert("cpf correto");
-							return true;   
+							v = true;
+							return true;
 						}
 					}
+					return true;
 				} // end cpf validation
 			});
 				
